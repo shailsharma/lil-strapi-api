f00623e38e9c3d977d25bae6f053041c5c1b1cec
Pradip Hudekar <phudekar@equalexperts.com>
2016-02-09 18:19:51 +0530

    corrected docker push commands

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 docker build -t lil-strapi-api .
 
 # tag image
-docker tag lilapi/lil-strapi-api:latest lilapi/lil-api:latest
-#docker tag lilapi/lil-strapi-api:latest lilapi/lil-strapi-api:build-$CIRCLE_BUILD_NUM
+docker tag lil-strapi-api:latest lilapi/lil-strapi-api:latest
+#docker tag lil-strapi-api:latest lilapi/lil-strapi-api:build-$CIRCLE_BUILD_NUM
 
-docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD -e=$DOCKER_EMAIL tutum.co
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD -e=$DOCKER_EMAIL
 docker push lilapi/lil-strapi-api:latest
 #docker push lilapi/lil-strapi-api:build-$CIRCLE_BUILD_NUM
 
