e55f5f126db0130e05d89ab90f2e37ad9bd6446f
gitesh-purbia <gitesh.purbiya22@gmail.com>
2016-02-09 18:40:03 +0530

    added trigger url

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,4 @@ docker push lilapi/lil-strapi-api:latest
 
 
 # trigger deployment
+curl -X POST https://dashboard.tutum.co/api/v1/service/4edf2588-2eed-4d35-969f-5531f9723b18/trigger/b9f7ef65-d04f-42ec-b166-2a9480ba58ac/call/
