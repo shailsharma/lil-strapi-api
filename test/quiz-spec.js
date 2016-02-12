8d4a1dee29dc296f1db631ba52d127793bd7b812
Pradip Hudekar <phudekar@equalexperts.com>
2016-02-12 15:26:21 +0530

    added some wait before running tests

--- test/quiz-spec.js
+++ test/quiz-spec.js
@@ -5,23 +5,22 @@ var request = require('request');
 chai.use(chaiHttp);
 
 describe('Quiz Model Test', function(){
-    
+
     it('Should match values', function(){
         var name = 'Gitesh Purbia';
         expect(name).to.equal('Gitesh Purbia');
     });
-    
+
     it('Should have a status code 200', function(done) {
         chai.request('http://localhost:1337')
             .get('/quiztest')
             .end(function(err, res){
-                console.log(res);
                 expect(res.status).to.equal(200);
                 expect(res.body).to.be.a('array');
                 done();
             });
     });
-    
+
     it('should return 404', function (done) {
         chai.request('http://localhost:1337')
             .get('/wrongurl')
