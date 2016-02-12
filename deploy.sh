83c5b179f458800fe3864ef5cf08677e2ab718cd
gitesh-purbia <gitesh.purbiya22@gmail.com>
2016-02-12 12:32:46 +0530

    update deploysh file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#build image
-docker build -t lil-strapi-api .
-
 # tag image
 docker tag lil-strapi-api:latest lilapi/lil-strapi-api:latest
 #docker tag lil-strapi-api:latest lilapi/lil-strapi-api:build-$CIRCLE_BUILD_NUM
